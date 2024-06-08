7f58c82c44cd92bfc0e968c6cdd60dd92a56551b
diegohyenna <dgmartins87@hotmail.com>
2024-06-08 11:15:35 -0300

    feat: implementado botões da parte superior da pagina

--- src/app/pages/ucs/ucs.component.ts
+++ src/app/pages/ucs/ucs.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {
+  Breadcrumbs,
+  HeaderService,
+} from './../../shared/components/header/header.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-ucs',
   templateUrl: './ucs.component.html',
-  styleUrls: ['./ucs.component.scss']
+  styleUrls: ['./ucs.component.scss'],
 })
-export class UcsComponent {
+export class UcsComponent implements OnInit {
+  constructor(private headerService: HeaderService) {}
 
+  ngOnInit(): void {
+    const breadcrumbs: Breadcrumbs[] = [{ name: 'Gestão de UCs' }];
+    this.headerService.setBreadcrumbs(breadcrumbs);
+  }
 }

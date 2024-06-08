dd24f9a79526cb9a97e58dd8cfaa145eefd81687
diegohyenna <dgmartins87@hotmail.com>
2024-06-08 08:49:49 -0300

    refactor: importação de modulo router

--- src/app/shared/shared-components.module.ts
+++ src/app/shared/shared-components.module.ts
@@ -4,10 +4,11 @@ import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { IconSvgComponent } from './components/icon-svg/icon-svg.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [SidenavComponent, MenuComponent, IconSvgComponent],
   exports: [SidenavComponent, MenuComponent, IconSvgComponent],
-  imports: [CommonModule, FontAwesomeModule],
+  imports: [CommonModule, FontAwesomeModule, RouterModule],
 })
 export class SharedComponentsModule {}

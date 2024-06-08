9710755b2e9f145710ed6aa98864dfc68a1b26c5
diegohyenna <dgmartins87@hotmail.com>
2024-06-08 09:44:24 -0300

    feat: criação do componente header

--- src/app/shared/shared-components.module.ts
+++ src/app/shared/shared-components.module.ts
@@ -5,10 +5,16 @@ import { MenuComponent } from './components/menu/menu.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { IconSvgComponent } from './components/icon-svg/icon-svg.component';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [SidenavComponent, MenuComponent, IconSvgComponent],
-  exports: [SidenavComponent, MenuComponent, IconSvgComponent],
+  declarations: [
+    SidenavComponent,
+    MenuComponent,
+    IconSvgComponent,
+    HeaderComponent,
+  ],
+  exports: [SidenavComponent, MenuComponent, IconSvgComponent, HeaderComponent],
   imports: [CommonModule, FontAwesomeModule, RouterModule],
 })
 export class SharedComponentsModule {}

a7a338103745ff15be342b4255b4f52943ee8302
diegohyenna <dgmartins87@hotmail.com>
2024-06-09 11:56:34 -0300

    fix: retirado componente do lugar errado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { UcsComponent } from './pages/ucs/ucs.component';
 import { SharedComponentsModule } from './shared/shared-components.module';
 
 @NgModule({
-  declarations: [AppComponent, UcsComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,

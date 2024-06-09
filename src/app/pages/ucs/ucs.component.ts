eaef5bb97b083e983c1b866315b4da547cfe2e9c
diegohyenna <dgmartins87@hotmail.com>
2024-06-09 09:42:55 -0300

    refactor: terminado implementações no componente ucs

--- src/app/pages/ucs/ucs.component.ts
+++ src/app/pages/ucs/ucs.component.ts
@@ -11,10 +11,45 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./ucs.component.scss'],
 })
 export class UcsComponent implements OnInit {
+  totalUC?: number;
+  status?: { actives: number; inactives: number };
+  type?: {
+    consumer: number;
+    gerator: number;
+  };
+  group?: { groupA: number; groupB: number };
+  invoicing?: {
+    withAddress: number;
+    noAddress: number;
+    withCpfCnpj: number;
+    noCpfCnpj: number;
+  };
+
   constructor(private headerService: HeaderService) {}
 
   ngOnInit(): void {
     const breadcrumbs: Breadcrumbs[] = [{ name: 'Gestão de UCs' }];
     this.headerService.setBreadcrumbs(breadcrumbs);
+
+    this.totalUC = 245;
+
+    this.status = { actives: 10, inactives: 0 };
+
+    this.type = {
+      consumer: 122,
+      gerator: 30,
+    };
+
+    this.group = {
+      groupA: 120,
+      groupB: 200,
+    };
+
+    this.invoicing = {
+      withAddress: 230,
+      noAddress: 0,
+      withCpfCnpj: 120,
+      noCpfCnpj: 300,
+    };
   }
 }

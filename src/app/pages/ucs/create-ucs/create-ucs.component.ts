0a9211907a6d8900c5a89e5486489f14342af9ca
diegohyenna <dgmartins87@hotmail.com>
2024-06-09 10:02:25 -0300

    feat: criada pagina create ucs onde vai conter o form de criação/edição

---
+++ src/app/pages/ucs/create-ucs/create-ucs.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-ucs',
+  templateUrl: './create-ucs.component.html',
+  styleUrls: ['./create-ucs.component.scss']
+})
+export class CreateUcsComponent {
+
+}

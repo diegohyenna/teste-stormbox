c1c506b1d933b5561fc0b425ed80109bda6d302f
diegohyenna <dgmartins87@hotmail.com>
2024-06-10 13:01:58 -0300

    chore: adiçaõ da lib ngx indexed db

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,69 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedComponentsModule } from './shared/shared-components.module';
 
+import { DBConfig, NgxIndexedDBModule } from 'ngx-indexed-db';
+
+const dbConfig: DBConfig = {
+  name: 'MyDb',
+  version: 1,
+  objectStoresMeta: [
+    {
+      store: 'ucs',
+      storeConfig: { keyPath: 'id', autoIncrement: true },
+      storeSchema: [
+        {
+          name: 'validityDate',
+          keypath: 'validityDate',
+          options: { unique: false },
+        },
+        {
+          name: 'concessionaire',
+          keypath: 'concessionaire',
+          options: { unique: false },
+        },
+
+        { name: 'uf', keypath: 'uf', options: { unique: false } },
+        {
+          name: 'unityNumber',
+          keypath: 'unityNumber',
+          options: { unique: false },
+        },
+        {
+          name: 'unityDescription',
+          keypath: 'unityDescription',
+          options: { unique: false },
+        },
+        {
+          name: 'clientNumber',
+          keypath: 'clientNumber',
+          options: { unique: false },
+        },
+        { name: 'company', keypath: 'company', options: { unique: false } },
+        {
+          name: 'consumerType',
+          keypath: 'consumerType',
+          options: { unique: false },
+        },
+        { name: 'modality', keypath: 'modality', options: { unique: false } },
+        { name: 'rural', keypath: 'rural', options: { unique: false } },
+        {
+          name: 'organizationType',
+          keypath: 'organizationType',
+          options: { unique: false },
+        },
+        {
+          name: 'connectionType',
+          keypath: 'connectionType',
+          options: { unique: false },
+        },
+        { name: 'cpfCnpj', keypath: 'cpfCnpj', options: { unique: false } },
+        { name: 'email', keypath: 'email', options: { unique: false } },
+        { name: 'password', keypath: 'password', options: { unique: false } },
+      ],
+    },
+  ],
+};
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -15,6 +78,7 @@ import { SharedComponentsModule } from './shared/shared-components.module';
     FontAwesomeModule,
     BrowserAnimationsModule,
     SharedComponentsModule,
+    NgxIndexedDBModule.forRoot(dbConfig),
   ],
   providers: [],
   bootstrap: [AppComponent],

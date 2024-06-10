4e2edbf08b49379b9a4d60c4f3c8d840abf06359
diegohyenna <dgmartins87@hotmail.com>
2024-06-10 13:03:18 -0300

    refactor: implementação de novos metodos do serviço da api

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -1,6 +1,58 @@
 import { Injectable } from '@angular/core';
+import { NgxIndexedDBService } from 'ngx-indexed-db';
+
+export interface UC {
+  id: string;
+  validityDate: string;
+  concessionaire: {
+    id: number;
+    name: string;
+  };
+  uf: { id: number; name: string };
+  unityNumber: string;
+  unityDescription: string;
+  clientNumber: string;
+  company: string;
+  consumerType: { id: number; name: string };
+  modality: {
+    id: number;
+    name: string;
+  };
+  rural: {
+    id: number;
+    name: string;
+  };
+  organizationType: {
+    id: number;
+    name: string;
+  };
+  connectionType: {
+    id: number;
+    name: string;
+  };
+  cpfCnpj: string;
+  email: string;
+  password: string;
+}
 
 @Injectable({ providedIn: 'root' })
 export class ApiService {
-  constructor() {}
+  store = 'ucs';
+  constructor(private dbService: NgxIndexedDBService) {}
+
+  getAll() {
+    return this.dbService.getAll(this.store);
+  }
+
+  getById(ucId: number) {
+    return this.dbService.getByID(this.store, ucId);
+  }
+
+  create(uc: UC) {
+    return this.dbService.add(this.store, uc);
+  }
+
+  update(uc: UC) {
+    return this.dbService.update(this.store, uc);
+  }
 }

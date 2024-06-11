717f8106a04e942c5829052877e1ad9dfeeb5b5d
diegohyenna <dgmartins87@hotmail.com>
2024-06-11 13:05:25 -0300

    refactor: alterações para a pagina de notfound

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,10 +1,15 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss']
+  styleUrls: ['./not-found.component.scss'],
 })
 export class NotFoundComponent {
+  constructor(private location: Location) {}
 
+  goBack() {
+    this.location.back();
+  }
 }

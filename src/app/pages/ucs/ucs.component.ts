3bf4a4868e355a697d7d51b855b51cc6aa628492
diegohyenna <dgmartins87@hotmail.com>
2024-06-11 12:55:45 -0300

    refactor: finalizado pagina principal ucs

--- src/app/pages/ucs/ucs.component.ts
+++ src/app/pages/ucs/ucs.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
+import { StatusModalComponent } from 'src/app/shared/components/modal/status/status.component';
 import { ApiService, UC } from 'src/app/shared/services/api.service';
 
-import {
-  Alert,
-  AlertService,
-} from './../../shared/components/alert/alert.service';
+import { Alert } from './../../shared/components/alert/alert.service';
 import {
   Breadcrumbs,
   HeaderService,
@@ -23,11 +22,13 @@ export class UcsComponent implements OnInit {
     gerator: number;
   };
   group?: { groupA: number; groupB: number };
-  invoicing?: {
-    withAddress: number;
-    noAddress: number;
-    withCpfCnpj: number;
-    noCpfCnpj: number;
+  address?: {
+    with: number;
+    without: number;
+  };
+  cpfCnpj?: {
+    with: number;
+    without: number;
   };
 
   alert?: Alert | undefined;
@@ -37,38 +38,97 @@ export class UcsComponent implements OnInit {
   constructor(
     private headerService: HeaderService,
     private apiService: ApiService,
-    private alertService: AlertService
+    private modalService: BsModalService
   ) {}
 
   ngOnInit(): void {
     const breadcrumbs: Breadcrumbs[] = [{ name: 'Gestão de UCs' }];
     this.headerService.setBreadcrumbs(breadcrumbs);
+    this.initializeUcs();
+  }
 
-    this.totalUC = 245;
-
-    this.status = { actives: 10, inactives: 0 };
-
-    this.type = {
-      consumer: 122,
-      gerator: 30,
-    };
-
-    this.group = {
-      groupA: 120,
-      groupB: 200,
-    };
-
-    this.invoicing = {
-      withAddress: 230,
-      noAddress: 0,
-      withCpfCnpj: 120,
-      noCpfCnpj: 300,
-    };
-
+  initializeUcs() {
     this.apiService.getAll().subscribe({
       next: (ucs: any) => {
         this.ucs = ucs;
+
+        this.countUCs(ucs);
+        this.countConsumersType(ucs);
+        this.countGroups(ucs);
+        this.countCpfCnpj(ucs);
+        this.countStatus(ucs);
       },
+    });
+  }
+
+  countUCs(ucs: UC[]) {
+    this.totalUC = ucs.length;
+  }
+
+  countConsumersType(ucs: UC[]) {
+    this.type = {
+      consumer: 0,
+      gerator: 0,
+    };
+    ucs.forEach((uc) => {
+      if (this.type && uc.consumerType.id == 1) this.type.consumer++;
+      if (this.type && uc.consumerType.id == 2) this.type.gerator++;
+    });
+  }
+
+  countGroups(ucs: UC[]) {
+    this.group = {
+      groupA: 0,
+      groupB: 0,
+    };
+    ucs.forEach((uc) => {
+      if (this.group && [1, 2, 3, 4, 5, 6, 7].includes(uc.modality.id))
+        this.group.groupA++;
+      if (this.group && [8, 9, 10, 11, 12].includes(uc.modality.id))
+        this.group.groupB++;
+    });
+  }
+
+  countCpfCnpj(ucs: UC[]) {
+    this.cpfCnpj = {
+      with: 0,
+      without: 0,
+    };
+    ucs.forEach((uc) => {
+      if (this.cpfCnpj && uc.cpfCnpj) this.cpfCnpj.with++;
+      if (this.cpfCnpj && !uc.cpfCnpj) this.cpfCnpj.without++;
+    });
+  }
+
+  countStatus(ucs: UC[]) {
+    this.status = {
+      actives: 0,
+      inactives: 0,
+    };
+    ucs.forEach((uc) => {
+      if (this.status && uc.status == 'Ativo') this.status.actives++;
+      if (this.status && uc.status == 'Inativo') this.status.inactives++;
+    });
+  }
+
+  modal!: BsModalRef;
+  openStatusModal(uc: UC) {
+    const initialState: ModalOptions = {
+      initialState: {
+        uc: uc,
+      },
+    };
+    this.modal = this.modalService.show(StatusModalComponent, initialState);
+
+    this.modal.content.onHide.subscribe((result: boolean) => {
+      if (result) this.modal.hide();
+      this.initializeUcs();
+    });
+  }
+
+  onDelete(ucId: number) {
+    this.apiService.delete(ucId).subscribe(() => {
+      this.initializeUcs();
     });
   }
 }
